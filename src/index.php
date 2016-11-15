8e1c5b4888c240f36e294ed84c57f7077a3bf33d
Guy Romm <guyromm@guyromm.com>
2016-11-16 00:16:42 +0200

    first baby steps

--- src/index.php
+++ src/index.php
@@ -1,4 +1,12 @@
 <h1>hi there mofos.</h1>
-<? for ($i=0;$i<10;$i+=1) { ?>
-<?=$i?><br />
+<?
+$entries = array_filter(scandir('src/entries/'),function($key) {
+	 if (preg_match('#\.php$#',$key)) return true;
+	 });
+?>
+<ul>
+<? foreach ($entries as $e) { ?>
+<li><a href="/entries/<?=$e?>"><?=htmlspecialchars(preg_replace('#\.php$#','',$e))?></a></li>
 <? } ?>
+</ul>
+would you like to see our <a href="pth/phpinfo.php">phpinfo?</a>
